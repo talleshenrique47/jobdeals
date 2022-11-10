1220f4ae35af69308ebb119765ea579b781e64cc
Talles Henrique da Silva <talles.henrique47@hotmail.com>
2022-11-09 21:27:24 -0300

    mask cnpj

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -4,6 +4,8 @@ const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmpassword = document.getElementById('confirmpassword');
+const cnpj = document.getElementById('cnpj');
+
 
 
 function validate(item){
@@ -17,10 +19,19 @@ function validate(item){
     }
 }
 
+function maskCNPJ() {
+    let strCNPJ = cnpj.value;
+    if (strCNPJ.length == 2 || strCNPJ.length == 6) {cnpj.value += "."}
+    else if (strCNPJ.length == 10) {cnpj.value += "/"}
+    else if (strCNPJ.length == 15) {cnpj.value += "-"}
+}
+
+
 //Solicita validação a cada alteração na entrada
 firstname.addEventListener('input', function() {validate(firstname)});
 lastname.addEventListener('input', function() {validate(lastname)});
 email.addEventListener('input', function() {validate(email)});
 password.addEventListener('input', function() {validate(password)});
 confirmpassword.addEventListener('input', function() {validate(confirmpassword)});
+cnpj.addEventListener('input', function() {maskCNPJ()});
 
